2b4e40fbaa7fc1e48c66ebb546b659c31bf23a58
adrfig966 <adrfig96@gmail.com>
2026-02-18 12:07:04 -0800

    Bumped version number. Removed border-top from detail list.

--- includes/updater.php
+++ includes/updater.php
@@ -21,7 +21,7 @@ class Updater {
 
         //$this->plugin_slug   = dirname ( plugin_basename( __DIR__ ) );
         $this->plugin_slug   = 'listings-api';
-        $this->version       = '2.3';
+        $this->version       = '2.35';
         $this->cache_key     = 'legacy-listings-api-updater';
         $this->cache_allowed = false;
 
